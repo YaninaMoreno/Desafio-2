68170e94b1c7debea3c8c1defc6bb639fafde11c
YaniMoreno <yani.elbolson@gmail.com>
2022-09-05 23:19:44 -0300

    incorporar arrays

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-// SIMULADOR INTERACTIVO
+//INCORPORANDO ARRAYS
 
-const Scotch = 2000;
-const Honey = 1500;
-const IPA = 3000;
-const Red = 2300;
-const APA = 6000;
-const pilsner = 7000;
+const tienda = [{"Scotch" : 2000}, {"Honey" : 1500}, {"IPA" : 3000}, {"Red" : 2300}, {"APA" : 6000}, {"pilsner" : 7000}];
 
+ 
 let compraProducto = (mostrar) => {
     alert("¡Bienvenido a Mountain Beer!");
     let limite = parseInt(prompt("¿Cuantos productos desea comprar?"));
@@ -18,17 +14,17 @@ let compraProducto = (mostrar) => {
     for (i = 1; i <= limite; i++) {
         let cerveza = prompt("Tenemos \n 1. Scotch \n 2. Honey \n 3. IPA \n 4. Red \n 5. APA \n 6. Pilsner");
         if (cerveza == "1") {
-            console.log("$" + Scotch)
+            console.log("El precio de la cerveza es de $" + tienda[0].Scotch)
         } else if (cerveza == "2") {
-            console.log("$" + Honey)
+            console.log("El precio de la cerveza es de $" + tienda[1].Honey)
         } else if (cerveza == "3") {
-            console.log("$" + IPA)
+            console.log("El precio de la cerveza es de $" + tienda[2].IPA)
         } else if (cerveza == "4") {
-            console.log("$" + Red)
+            console.log("El precio de la cerveza es de $" + tienda[3].Red)
         } else if (cerveza == "5") {
-            console.log("$" + APA)
+            console.log("El precio de la cerveza es de $" + tienda[4].APA)
         } else if (cerveza == "6") {
-            console.log("$" + pilsner)
+            console.log("El precio de la cerveza es de $" + tienda[5].pilsner)
         }
     }
     alert("¡Gracias por comprar en Mountain Beer!")
